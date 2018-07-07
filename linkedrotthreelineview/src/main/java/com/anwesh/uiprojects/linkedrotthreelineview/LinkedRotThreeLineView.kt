110e26aeca3c3b0412d6b6a5e1b47887c7a52caf
Anwesh43 <anweshthecool0@gmail.com>
2018-07-07 09:44:12 +0530

    adding LinkedRotThreeView class

--- linkedrotthreelineview/src/main/java/com/anwesh/uiprojects/linkedrotthreelineview/LinkedRotThreeLineView.kt
+++ linkedrotthreelineview/src/main/java/com/anwesh/uiprojects/linkedrotthreelineview/LinkedRotThreeLineView.kt
@@ -3,3 +3,26 @@ package com.anwesh.uiprojects.linkedrotthreelineview
 /**
  * Created by anweshmishra on 07/07/18.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.content.Context
+
+class LinkedRotThreeLineView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
