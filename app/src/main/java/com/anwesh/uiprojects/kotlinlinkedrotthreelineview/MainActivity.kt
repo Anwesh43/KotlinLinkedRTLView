09e2d39972d8ae09b2006e22c118925dd7af22aa
Anwesh43 <anweshthecool0@gmail.com>
2018-07-07 17:00:24 +0530

    using onCompletionListener

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedrotthreelineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedrotthreelineview/MainActivity.kt
@@ -3,13 +3,16 @@ package com.anwesh.uiprojects.kotlinlinkedrotthreelineview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedrotthreelineview.LinkedRotThreeLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedRotThreeLineView.create(this)
+        val view : LinkedRotThreeLineView = LinkedRotThreeLineView.create(this)
+        view.addOnCompletionListener({ Toast.makeText(this,"${it} completed",Toast.LENGTH_SHORT).show()},
+                { Toast.makeText(this,"${it} is reset",Toast.LENGTH_SHORT).show()})
         fullScreen()
     }
 }
