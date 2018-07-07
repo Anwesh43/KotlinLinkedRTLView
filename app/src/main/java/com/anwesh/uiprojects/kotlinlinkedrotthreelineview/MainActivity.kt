2c1b4fa5c2163b1836e328c7da2eaf51732166ec
Anwesh43 <anweshthecool0@gmail.com>
2018-07-07 11:41:38 +0530

    adding View in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedrotthreelineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedrotthreelineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedrotthreelineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedrotthreelineview.LinkedRotThreeLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedRotThreeLineView.create(this)
     }
 }
