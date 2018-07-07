5c7d27a82ea469a43e2e0c12ebf910823d524375
Anwesh43 <anweshthecool0@gmail.com>
2018-07-07 10:25:27 +0530

    adding State class

--- linkedrotthreelineview/src/main/java/com/anwesh/uiprojects/linkedrotthreelineview/LinkedRotThreeLineView.kt
+++ linkedrotthreelineview/src/main/java/com/anwesh/uiprojects/linkedrotthreelineview/LinkedRotThreeLineView.kt
@@ -25,4 +25,24 @@ class LinkedRotThreeLineView (ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
+
+        fun update(stopcb : (Float) -> Unit) {
+            scale += 0.1f * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                prevScale = scale + dir
+                dir = 0f
+                scale = prevScale
+                stopcb(prevScale)
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
 }
